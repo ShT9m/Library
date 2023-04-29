8b9818629b16e96fb99fc1787a63e05a6fd850d4
ShT9m <tk.sn9m@gamil.com>
2023-04-29 18:21:36 +0900

    Initial Commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
     <div class="row">
         <div class="col me-3 h1 border shadow rounded d-flex align-items-center justify-content-center" style="height: 100px">
             {{-- @include Authors --}}
-            <a href="{{ route('author.create') }}" class="text-decoration-none text-primary fw-bold">
+            <a href="{{ route('author.create') }}" class="text-decoration-none text-primary fw-bold bg-success">
                 <i class="fa-solid fa-users me-1"></i>Author
                 <span>{{ $authors->count() }}</span>
             </a>
